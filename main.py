48e91b8dcc2fad31d7ce3dbab092c97bb7c89369
Lena Skorokhodova <87618005+HelBroone@users.noreply.github.com>
2022-05-18 16:10:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import datetime
+from _datetime import datetime
 
 import telebot
 from pycbrf import ExchangeRates
 
 bot = telebot.TeleBot('5158217850:AAF6FhYM9bD24LIR67QDI6MDsAMYOyaG9hc')
 
-USD = 65.38
-'''нужно подключение к реальному курсу'''
-EUR = 68.94
+
 
 @bot.message_handler(commands=['start']) #отслеживание команды старта и вообще команд
 def start(message):
@@ -24,64 +22,78 @@ def help(message):
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.text == '/transfer':
-        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row('€', '$')
-        msg = bot.send_message(message.chat.id, 'Выберите валюту',
-                               reply_markup=markup)
-        bot.register_next_step_handler(msg, currency)
+        markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+        but1 = telebot.types.KeyboardButton('USD')
+        but2 = telebot.types.KeyboardButton('EUR')
+        markup.add(but1, but2) #создание кнопок
+        msg = bot.send_message(chat_id=message.chat.id, text=f'Выберите валюту', reply_markup=markup, parse_mode='html')#вывод на экран предложения о переводе валюты
+        bot.register_next_step_handler(msg, currency) #для того чтобы вызывать другие функции
 
 
 def currency(message):
-    if message.text == '€':
+    if message.text == 'EUR':
         msg = bot.send_message(message.chat.id, "Введите сумму в рублях")
-        bot.register_next_step_handler(msg, eur)
-    elif message.text == '$':
+        bot.register_next_step_handler(msg, course)
+    elif message.text == 'USD':
         msg = bot.send_message(message.chat.id, "Введите сумму в рублях")
-        bot.register_next_step_handler(msg, usd)
-
+        bot.register_next_step_handler(msg, course)
     else:
         msg = bot.send_message(message.chat.id, "Введите корректные данные")
         bot.register_next_step_handler(msg, currency)
 
+def course(message): #функция находжения курса
+    mess_norm = message.text.strip().lower()
+    bot.send_message(chat_id=message.chat.id, mess_norm)
 
+    if mess_norm == 'usd': #если мы выбрали что-то из этого
+        rates = ExchangeRates(datetime.now())
+        res = float(rates[mess_norm.upper()].rate) #это сам курс доллара
+        bot.send_message(message.chat.id, float(message.text))
+    elif mess_norm == 'eur':
+        rates = ExchangeRates(datetime.now())
+        res = float(rates[mess_norm.upper()].rate) #это сам курс евро
+        bot.send_message(message.chat.id, float(message.text))
+    else:
+        bot.send_message(message.chat.id, "Введите число")
+'''
 def eur(message):
     try:
-        bot.send_message(message.chat.id, float(message.text) / EUR)
+        bot.send_message(message.chat.id, float(message.text))
     except ValueError:
         bot.send_message(message.chat.id, "Введите число")
 
 
 def usd(message):
     try:
-        bot.send_message(message.chat.id, float(message.text) / USD)
+        bot.send_message(message.chat.id, float(message.text))
     except ValueError:
         bot.send_message(message.chat.id, "Введите число")
 
-
+'''
 bot.polling(none_stop=True)
 
 @bot.message_handler(commands=['quit']) #функция выхода из бота в дефолтное состояние или это не функция должна быть
 def quit(message):
     mess = f'/transfer - команда перевода валюты'
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+#код получения курса в реальном времени и его вывода в боте
 '''
-@bot.message_handler(commands=['start']) #отслеживание команды старта и вообще команд
-def start(message):
+@bot.message_handler(commands=['course']) #отслеживание команды старта и вообще команд
+def course(message):
     markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
     it1 = telebot.types.KeyboardButton('USD')
     it2 = telebot.types.KeyboardButton('EUR')
     markup.add(it1,it2)
-    mess = f'Выберите валюту'
-    bot.send_message(message.chat.id, mess, reply_markup=markup, parse_mode='html') #чтобы бот отправлял сообщение Привет
+    bot.send_message(chat_id= message.chat.id, text=f'Выберите валюту', reply_markup=markup, parse_mode='html') #чтобы бот отправлял сообщение Привет
 
 @bot.message_handler(content_types=['text'])
 def message(message):
     mess_norm = message.text.strip().lower()
     if mess_norm in ['usd','eur']:
         rates = ExchangeRates(datetime.now())
-        mess = f'Курс такой {float(rates)}'
-        bot.send_message(message.chat.id, mess, parse_mode='html')
+        bot.send_message(chat_id=message.chat.id, text=f'<b>{mess_norm.upper()} rate is {float(rates[mess_norm.upper()].rate)}</b>', parse_mode='html')
 
 
-bot.polling()
+bot.polling(none_stop=True)
 '''
