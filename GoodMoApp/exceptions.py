27e411772bd0adbc2b0910bf153dea07227fc614
Lena Skorokhodova <87618005+HelBroone@users.noreply.github.com>
2022-06-04 03:23:54 +0300

    Delete GoodMoApp directory

--- GoodMoApp/exceptions.py
+++
@@ -1,3 +0,0 @@
-class ConvertionExeption(Exception):
-    """Description: Raise an error, when something goes wrong."""
-    pass
--- GoodMoApp/texts.py
+++
@@ -1,5 +0,0 @@
-class KeyboardTexts:
-    """Class of button's text."""
-    WEATHER = "🌥 Погода"
-    CURRENCY_RATE = "💸 Курсы валют"
-    EXCHANGE_CURRENCY = "💹 Перевод валют"
